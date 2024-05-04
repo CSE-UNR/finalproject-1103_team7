56604d5f7edf0615590ad42a4bc126a50fe21711
1103-Hur-Suebin <lvsuebin@gmail.com>
2024-05-04 02:30:29 +0000

    meow

--- finalproject.c
+++ finalproject.c
@@ -1,4 +1,62 @@
 /* 
 Authors: Suebin Hur and Raina Santos
-
+Purpose: Final Group Project 
+Date: May, 07 2024 
 */
+
+#include <stdio.h> 
+#include <stdbool.h>
+
+//function prototypes here 
+
+int main(){
+//declare variables 
+
+
+//main menu loop 
+
+
+
+
+//enter switch for diff cases using functions made 
+
+
+
+
+}
+
+
+//functions start here !!  
+//1. display menu function
+int mainMenu(){
+	int choice; 
+	printf("**ERINSTAGRAM**\n"); 
+	printf("1: Load image\n");
+	printf("2: Display image\n");
+	printf("3: Edit image\n");
+	printf("0: Exit \n"); 
+	printf("\n");
+	printf("Choose from one of the options above: ");
+	scanf("%d", &choice); 
+	return 0; 
+}
+
+//2. load image function 
+
+
+//3. display image fuction
+
+
+//5. crop image function 
+
+
+//6. dim image function 
+
+
+//7. brighten image function 
+
+
+//8. save image function 
+
+
+//9. extra credit: rotate image function 
